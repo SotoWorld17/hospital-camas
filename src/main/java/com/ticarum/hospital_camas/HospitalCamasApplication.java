74a77fa3f78f59376e7a3ce859db396faf5d847e
SotoWorld <72091639+SotoWorld17@users.noreply.github.com>
2025-05-30 20:22:33 +0200

    Subida inicial del proyecto

---
+++ src/main/java/com/ticarum/hospital_camas/HospitalCamasApplication.java
@@ -0,0 +1,13 @@
+package com.ticarum.hospital_camas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalCamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalCamasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticarum/hospital_camas/HospitalCamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticarum.hospital_camas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
